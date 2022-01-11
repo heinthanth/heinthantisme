4e4326638648bc507fa0af06d18496edeafff25b
Hein Thant Maung Maung <me@heinthanth.com>
2022-01-11 13:24:53 +0630

    optimize codes

--- src/components/noscript.tsx
+++ src/components/noscript.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 const NoScriptWrapper = ({ shouldWrap, children }: { shouldWrap: boolean; children: ReactNode }) =>
   shouldWrap ? <noscript>{children}</noscript> : <>{children}</>;
