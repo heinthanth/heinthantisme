0babc1d196ec05b937f10776e31dfed6004d0d03
Hein Thant Maung Maung <me@heinthanth.com>
2022-01-22 11:58:53 +0630

    drop png support. use webp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,18 +4,17 @@ import { NextSeo } from "next-seo";
 import cx from "classnames";
 import commonCss from "../components/common.module.sass";
 import homeCss from "./home.module.sass";
-import Image from "../components/image";
 
-import uitOc from "../assets/images/home-page/ht-uit-oc.png";
-import uitOcWebp from "../assets/images/home-page/ht-uit-oc.webp";
-import uitOcLowq from "../assets/images/home-page/ht-uit-oc-0.5x.png";
-import uitOcWebpLowq from "../assets/images/home-page/ht-uit-oc-0.5x.webp";
+import uitOc0p5x from "../assets/images/home-page/ht-uit-oc@0.5x.webp";
+import uitOc from "../assets/images/home-page/ht-uit-oc@1x.webp";
+import uitOc2x from "../assets/images/home-page/ht-uit-oc@2x.webp";
 
 const HomePage = () => (
   <Fragment>
     <NextSeo title="Home" />
     <div className={cx(commonCss.section, homeCss.hero)}>
-      <Fade className="lg:grid grid-cols-2 gap-x-4 h-full container" delay={400}>
+      {/* FIXME: `.hero::before` wrong z position when using `div` instead of `Fade` */}
+      <Fade className="lg:grid grid-cols-2 gap-x-4 h-full container">
         <Fragment>
           <div className="flex flex-col justify-center mb-8 lg:mt-[-100px]">
             <small className="block text-sm mb-3">Hi there, It&apos;s me,</small>
@@ -26,15 +25,13 @@ const HomePage = () => (
             </p>
           </div>
           <div className="pr-0 lg:pl-16 flex flex-col justify-center mb-6 lg:mt-[-100px]">
-            <Image
-              pictureClass="ring-2 ring-black w-full mb-4 lg:mt-16 pointer-events-none select-none"
+            <img
+              className="ring-2 ring-black w-full mb-4 lg:mt-16 pointer-events-none select-none"
               src={uitOc}
-              srcWebp={uitOcWebp}
-              alt="Hein Thant at UIT Open Campus exploring others projects"
-              srcLowq={uitOcLowq}
-              webpLowq={uitOcWebpLowq}
-              width={1024}
-              height={1024}
+              srcSet={`${uitOc0p5x} 256w, ${uitOc} 512w, ${uitOc2x} 1024w`}
+              alt="Hein Thant at UIT Open Campus 2018"
+              width={512}
+              height={512}
             />
             <small className="text-neutral-400 text-xs block select-none">
               they, both, are my friends and have asked their permission to use here.
