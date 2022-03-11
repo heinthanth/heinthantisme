5af3fafbba229075b842178d94f482d162eb3446
Hein Thant Maung Maung <me@heinthanth.com>
2022-03-11 08:43:10 +0630

    change description

--- src/misc/constants.ts
+++ src/misc/constants.ts
@@ -7,7 +7,7 @@ export const siteURL = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:300
 export const searchWords =
   ["Hein Thant Maung Maung", "HIIIiN", "Hein Thanth", "heinthanth", "ဟိန်းသန့် မောင်မောင်", "ဟိန်းဟိန်း"];
 export const description =
-  "Just a CS student, Full-stack Web Developer, Junior DevOps, Open Source Lover, and passionated Language hacker, and also, love to create EDM, too!";
+  "Just a CS student, Full-stack Web Developer, Junior Flutter Developer, Junior DevOps, Open Source Lover, and passionated Language hacker, and also, love to create EDM, too!";
 export const coverLocation = { url: absURL(cover.toString()) };
 export const creatorName = "Hein Thant Maung Maung";
 export const creatorUsername = "heinthanth";
